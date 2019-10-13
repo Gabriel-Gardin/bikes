3e1d3c0fa7fd83f9159c06f2965f551f01cda760
Gabriel Gardin <gabrielgardin96@gmail.com>
2019-10-13 09:13:34 -0300

    MQTT, JSON, ESTÁVEL.

--- include/modules.h
+++ include/modules.h
@@ -2,13 +2,41 @@
 #include <Arduino.h>
 #include "TinyGPS.h"
 #include <MKRGSM.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 TinyGPS gps;
  
 GSMClient client;
 GPRS gprs;
 GSM gsmAccess;
-  
+
+//Define um objeto utilizando a lib json para enviar os dados via mqtt. 120 é a RAM separada para alocar os dados.
+StaticJsonDocument<120> mqtt_json_data;
+
+//Add um array ao mqtt_data. Dados do gps
+JsonArray gps_json_data = mqtt_json_data.createNestedArray("GPS_Data"); 
+
+//tópico do MQTT 
+char* mqtttopic = "/ctu/bike1";
+
+//Servidor MQTT
+char* mqttserver = "co2incubator.xyz";
+
+//usuário configurado no broker com permissão para enviar dados.
+const char* mqttUser = "estufa1";
+
+//Senha do usuário client1 do mqtt
+const char* mqttPassword = "co2incubator132";
+
+//millis() desde o ultimo envio de dados
+unsigned long mqtt_time_send = 0;
+
+//Millis desde a ultima vez que checaram os sensores.
+unsigned long check_time = 0;
+
+PubSubClient mqttclient(client); //Instância da classe do Pubcubcliente passando o construtor "client" do GSM
+
 
 #define SerialGPS Serial1  // RX&TX PINS
 //#define SerialGPS Serial3 // SCL&SDA PINS
@@ -21,19 +49,25 @@ const uint8_t  radar_pin = 5;
 const uint8_t AVERAGE = 4; 
 
 //Pino conectado ao pino 2 (pwm) do módulo ultrasonico maxbotix. 
-#define ultrasonic_pin 4
+const uint8_t ultrasonic_pin = 4;
 
 //Verifica os dados do GPS através da serial UART do módulo 6neo m e retorna um ponteiro para um array contendo 4 parametros; Latitude, Longitude, numero satelite, e a velocidade da BICICLETA. 
-String * gps_data(); 
+String *get_gps_data();
 
 // Função chamada para ler a frequência de saída do módulo radar CDM324. Retorna a velocidade do objeto que estiver se aproximando em km/h 
 float get_cars_speed();
 
-//Função que conecta o gsm para ter acesso a rede de dados móveis. 
+//Função que conecta o gsm para ter acesso a rede de dados móveis e tbm conecta ao broker MQTT. 
 void gsm_connect_func();
 
 //Função chamada para ler o sensor ultrasônico da MaxBotix. Pode ser chamada 10 vezes por segundo. Retorna a distância em centimetros. 
 float get_distance();
+
+//Reconecta ao mqtt broker.
+void mqtt_reconect();
+
+//Função que envia os dados para o servidor por mqtt.
+int send_data(float speed, float distance, String *gpss);
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
    Utiliza arduino MKR 1400 na IDE Vs Code e PlatformIO. 
 */
 
-
 void setup() {
   pinMode(radar_pin, INPUT);
   pinMode(ultrasonic_pin, INPUT);
@@ -12,42 +11,101 @@ void setup() {
   SerialGPS.begin(9600); //TEST GPS.
   //delay(1000);
   gsm_connect_func();  //Connect to GPRS
-
 }
 
 void loop()
-{
+{//CHECAR O TEMPO DESDE A ULTIMA VEZ QUE LI OS SENSORES
+  float car_distance = 0;
+  float car_speed = 0;
+  int send;
+
   String * received_gps;
-  float dist = get_distance();
-  Serial.print("Distancia ");
-  Serial.println(dist);
-  Serial.println();
-  delay(500);
 
-  Serial.print("Is connected: ");
-  Serial.println(gprs.getIPAddress());
-  delay(500);
-
-  Serial.print("Velocidade: ");
-  Serial.print(get_cars_speed());
-  Serial.println("km/h");
-  Serial.println();
-  delay(500);
-
-  received_gps = gps_data();
-  for(int i = 0; i < 4; i++){
-    Serial.print(" ");
-    Serial.print(received_gps[i]);
-    delay(100);
+  if(millis() - mqtt_time_send > 2000) //Se não houve nenhum evento de carros em 15s envia a posição do gps com velocidade e distancia 0
+  {
+    received_gps = get_gps_data();
+    car_speed = 0;
+    car_distance = 0;
+    send = send_data(car_speed, car_distance, received_gps);
+    if(send == 1)
+    {
+      mqtt_time_send = millis();
+      delay(100); //Trocar para millis?
+    }
+    else
+    {
+      mqtt_reconect();
+    }
   }
-  Serial.println();
 
+  if(millis() - check_time > 100)
+  {
+    check_time = millis();
+    car_speed = get_cars_speed();
+    if(car_speed > 5)
+    {
+      car_distance = get_distance();
+      car_speed = get_cars_speed();
+      if(car_distance < 100)
+      {
+        received_gps = get_gps_data();
+        Serial.print("Send: ");
+        send = send_data(car_speed, car_distance, received_gps);
+        delay(100);
+
+        if(send == 1)
+        {
+          mqtt_time_send = millis();
+        }
+        else
+        {
+          mqtt_reconect();
+        }
+
+        Serial.println(send);
+        Serial.print("IP: ");
+        Serial.println(gprs.getIPAddress());
+      }
+    }
+  }
+/*  Ao invés de manter a conexão desestabiliza, perdendo a conexão. Vai entender.
+  if(!mqttclient.loop())
+  {
+    Serial.println("Chamando pra reconectar");
+    mqtt_reconect();
+  }*/
 }
+
+
+int send_data(float speed, float distance, String *gpss)
+{
+//  Serial.print("speed");
+//  Serial.println(speed);
+//  Serial.print("distnace");
+//  Serial.println(distance);
+  mqtt_json_data["Distância"] = distance;
+  mqtt_json_data["Car_speed"] = speed;
+
+  for(int i = 0; i < 4; i++)
+  {
+    gps_json_data.add(gpss[i]);
+ //   Serial.println(gpss[i]);
+  }
+
+  char data[512];
+  serializeJson(mqtt_json_data, data);
+  int send_state = mqttclient.publish("/ctu/bike1", data);
+  Serial.print("MQTT STATE: ");
+  Serial.println(mqttclient.state());
+//  if(send_state == 0){gsm_connect_func();}
+  return(send_state);
+}
+
 
 float get_cars_speed()
 {
   noInterrupts();
-  pulseIn(radar_pin, HIGH);
+  pulseIn(radar_pin, HIGH, 100000);
   unsigned int pulse_length = 0;
   for (x = 0; x < AVERAGE; x++)
   {
@@ -78,7 +136,8 @@ float get_cars_speed()
   }
 }
 
-String * gps_data()
+
+String *get_gps_data()
 {
   static String gps_data[4];
 
@@ -105,6 +164,9 @@ String * gps_data()
       int satelite_number = gps.satellites();
       satelite_number == TinyGPS::GPS_INVALID_SATELLITES ? 0 : satelite_number;
       //Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+
+      char convert_flat[16];
+      sprintf(convert_flat,"%f", flat);
 
       gps_data[0] = flat;
       gps_data[1] = flon;
@@ -142,12 +204,34 @@ void gsm_connect_func(){
   {
     if ((gsmAccess.begin(pin) == GSM_READY) && (gprs.attachGPRS(apn, login, pwd) == GPRS_READY)) 
     {
-      connected = true;
+      mqttclient.setServer(mqttserver, 1883);
+      if (mqttclient.connect("arduinoclient", mqttUser, mqttPassword )) {
+        connected = true;
+      }
+      else{
+        Serial.println("Falha ao conectar no broker!!");
+        Serial.println(mqttclient.state());
+      }
     } 
     else 
     {
-      Serial.println("Falha na conexão!!!");
+      Serial.println("Falha na conexão GPRS!!!");
       delay(1000);
     }
   }
+}
+
+void mqtt_reconect()
+{
+  Serial.println("Reconectando");
+  if(gprs.status() == GPRS_READY)
+  {
+    mqttclient.connect("arduinoclient", mqttUser, mqttPassword);
+    delay(10);
+  }
+  else
+  {
+    gsm_connect_func();
+  }
+  
 }
